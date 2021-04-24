74bafe397a26acb90dc9d74131ebac926fd64d93
Philip Lambok <philiplambok71@gmail.com>
2021-04-24 17:14:47 +0700

    Add logic to define the port web.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,15 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello, Spark!");
+    }
+
+    static int getHerokuAssignedPort(){
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if(processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
     }
 }
